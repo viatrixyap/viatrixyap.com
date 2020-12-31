d344eebe0d0f53d591303c376c718ece17002fa6
Viatrix Yap <mail@viatrixyap.com>
2020-12-31 19:08:25 +0800

    fixed mobile vs desktop header

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){ //close hamburger menu
+        body.classList.remove('noScroll');
         header.classList.remove('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
@@ -15,6 +17,7 @@ btnHamburger.addEventListener('click', function(){
     }
 
     else { //open hamburger menu
+        body.classList.add('noScroll');
         header.classList.add('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-out');
